69c7e2dd23ec017c90ed1bae1957d989187029ab
Chamith Jayaweera <chamithlkml@gmail.com>
2024-03-01 11:26:05 +0000

    Fixing the code with pare programming session

---
+++ script.js
@@ -0,0 +1,61 @@
+$(document).ready(function() {
+    // We write all jQuery related code inside this function
+    let currentInput = "";
+    let memory = [];
+    let memoryIndex = null;
+    let display = $('#displayField');
+    
+    // This is how you execute a function when a html element with the id=addMemory is clicked
+    $('#addMemory').on('click', () => {
+        // .val() is the value of the input field
+        var calculation = display.val();
+        memory.push(calculation);
+        memoryIndex = memory.length - 1;
+        // Set the display field to the result of the calculation
+        display.val(eval(calculation));
+    });
+});
+
+    // var display = $('input[name="display"]').eq(0);
+
+    // const addToMemory = (calculation) => {
+    //     memory.push(calculation);
+    //     memoryIndex = memory.length - 1;
+    //     display.value = eval(calculation);
+    // };
+    
+
+    
+        
+    // const getPreviousCalculation = () => {
+    //     if (memoryIndex === null) {
+    //         return null;
+    //     } else if ((memoryIndex + 1) <= memory.length - 1) {
+    //         return memory[++memoryIndex];
+    //     } else {
+    //         return memory[memoryIndex];
+    //     }
+    // };
+    
+
+    // const getNextCalculation = () => {
+    //     if (memoryIndex === null) {
+    //         return null;
+    //     } else if ((memoryIndex - 1) >= 0) {
+    //         return memory[--memoryIndex];
+    //     } else {
+    //         return memory[memoryIndex];
+    //     }
+    // };
+    
+
+
+    // const clearMemory = () => {
+    //     memory = [];
+    //     memoryIndex = null;
+    //     display.value = "";
+    // };
+    
+
+
+
